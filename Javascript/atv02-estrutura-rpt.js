11d745e41c554e9df8b4761394a3732cf23b26e4
Gaby Neves <108809176+gabynevess@users.noreply.github.com>
2023-03-03 08:02:50 -0300

    Delete atv02-estrutura-rpt.js

--- Javascript/atv02-estrutura-rpt.js
+++
@@ -1,6 +0,0 @@
-let num = parseFloat(prompt('Digite um número.'));
-for(let i = 0; i <= num; i++){
-    if(num%i ==0){
-        document.getElementById('info').innerHTML +=  `O divisor desse númeor é: ${i}<br>`
-    }
-}
