459a8f130bcf88e27a538bfcf54a144d18fd83c3
Gaby Neves <108809176+gabynevess@users.noreply.github.com>
2023-03-03 08:02:15 -0300

    Delete atividade02.js

--- Javascript/atividade02.js
+++
@@ -1,4 +0,0 @@
-let tempConv;
-let temperatureAtual  = parseInt(prompt('Digite a temperatura em Celcius que deseja converter, por favor'));
-tempConv = (temperatureAtual * 1.8) + 32
-document.getElementById('info').innerHTML = `A temperatura ${temperatureAtual}°C, convertida em Fahrenheit é: ${tempConv}F`;
