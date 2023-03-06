6c70c2c7f948fffbb131a45f5fecb00bf979c680
Gaby Neves <108809176+gabynevess@users.noreply.github.com>
2023-03-06 09:44:43 -0300

    Delete exc3-sabado.js

--- Javascript/atividades.js/exc3-sabado.js
+++
@@ -1,8 +0,0 @@
-let numP = parseFloat(prompt('Digite um número com o símbolo da porcentagem.'))
-
-function num(numP){
-    result = numP/100
-
-    document.getElementById('info').innerHTML = (`O resultado é:<br> ${result}`)
-}
-num(numP);
