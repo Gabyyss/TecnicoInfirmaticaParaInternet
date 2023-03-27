54900113411997eb6135d92d5a7bc78b8790cc1b
Gaby Neves <108809176+gabynevess@users.noreply.github.com>
2023-03-27 12:44:27 -0300

    Add files via upload

---
+++ Array-atividade/script.js
@@ -0,0 +1,10 @@
+var objetos = Array('Cadeira', 'Impressora', 'Garfo');
+let btn01 = document.getElementById('btn01');
+let btn02 = document.getElementById('btn02');
+
+btn01.addEventListener('click', () => {
+    if()
+    alert("Informe um valor válido")
+});
+
+
